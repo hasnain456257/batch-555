0dd6aa5a19f011e3237bbaa2a84b152a3039b547
hasnain456257 <hasnainhossain97@gmail.com>
2025-09-29 14:38:28 +0600

    hlw im cristiano ronaldo

---
+++ index.py
@@ -0,0 +1,688 @@
+#  print('Hello world') # this is our first line code.
+
+#  a = 20
+
+#  print()
+
+#  sting type 
+
+#  "hello" "30"  """"false"""
+
+#  int float
+# 208 23.56
+
+# a = 'banladesh'
+
+#  b = 10
+#  c = 20
+# b = 10
+
+#  z = None
+# print(c + b)
+# print(a + str(z))
+
+
+# a = int(input('enter a number :'))
+# b = input('enter a number :')
+
+
+# # print(type(b))
+# # print (a + int(b))
+
+# a = '..........i Love Bangladesh.......' 
+# b = 'i.love.bangladesh'
+# print(a[0:])
+# print(a[-11:])
+
+# print(a.upper())
+# print(a.lower())
+# print(a.capitalize())
+# print(a.title())
+#
+# print(len(a))
+# print(a.index('b'))
+# print(a.find('b'))
+# print(a.replace('bangladesh' , 'india'))
+# print(a.count('a'))
+# print(a.swapcase())
+# print(a.split("."))
+# print(a.strip("."))
+
+# b = a.center(50,".")
+# b = a.center(50)
+# print(len(b))
+
+
+
+# a = input('enter your name :')
+# b = input('enter your age :')
+# c = input('enter your subject :')
+
+
+# '''my name is hasan, i am 18  years old, i am learing python'''
+# x = 'my name is {name}, i am {age}  years old, i am learing {subject}'
+# print(x.format(name=a,age=b,subject=c))
+
+# print(f'my name is {a}, i am {b} years old, i am learing {c}')
+
+# a = 'it\'s alright'
+# a = 'it\\s alright'
+# a = 'it\ns alright'
+# a = 'it\ts alright'
+# a = 'it \bs alright'
+# print(a)
+
+
+# a = 33
+# b = 20
+# print(a // b)
+
+# = += -=
+# a = 20
+# # a = 20  #a = a + 10 
+# # a = 20  #a = a - 10
+# # a = 20  #a = a * 10
+# # a = 20  #a = a / 10 
+# # a = 20  #a = a % 10
+
+# a %= 10   
+# print(a)
+
+# comperison operator
+# ==  != > <  >= <=
+
+# a = 10 
+# b = 10
+# print(a != b)
+# print(a == b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+
+# logical operator
+#  and or not 
+
+# a = 10
+# b = 32
+# c = 20
+
+# print(a > b and b > c)
+# print(a > b or b > c)
+# print(not(a > b and b > c))
+# print(not(a > b or b > c))
+
+#  identity operator 
+#  is is not
+
+# a = 10 
+# b = 10 
+# print(a is not b)
+
+# membership operator
+# in not in
+
+# a = "I Love Bangladesh"
+# print('Bangladesh' not in a)
+
+# bitwise operator
+
+# # & | ^ ~ << >>
+# print (7 | 4)
+# 0 = 0 0 0 0
+# 1 = 0 0 0 1
+# 2 = 0 0 1 0
+# 3 = 0 0 1 1
+# 4 = 0 1 0 0
+# 5 = 0 1 0 1
+# 6 = 0 1 1 0
+# 7 = 0 1 1 1
+
+# a = 10
+# b = 23
+
+# if a == b or a != b:
+#     print(a + b)
+# else:
+#     print('not equal')
+ 
+ 
+ 
+# if a == b:
+#     print(a + b)
+# elif a > b :
+#     print(a - b)
+# elif a < b :
+#     print(f'this is b > a result : {b - a}')
+# else:
+#     print('not equal')
+
+# if a is b:
+#      if a == 11:
+#           print(a + b)
+#      else:
+#           print("a is not 10")
+# elif a < b :
+#     if a == 10:
+#      print(f'this is b > a result : {b - a}')  
+#     else :
+#         print("a is not equal 11")  
+# else:
+#      print('no match found')
+
+
+# marks = int(input("enter the number :"))
+
+# if marks <= 100 and marks > 0 :
+#     if marks >= 80:
+#         print("a+")
+#     elif marks >= 70:
+#         print("a")
+#     elif marks >= 60:
+#         print("a-")
+#     elif marks >= 50:
+#         print("b+")
+#     elif marks >= 40:
+#         print("c")
+#     elif marks < 40:
+#         print("f")
+
+# else:
+#     print("invalid input")
+
+# print("building calculator :\n")
+
+# num1 = float(input("enter a number"))
+# num2 = float(input("enter another number : "))
+# operator = input("enter an operator (+,-,*,/,%) : ")
+
+# if (operator == '+'):
+#     print(f'summation is : ' , num1 + num2)
+# elif(operator == '-'):
+#     print(f'summation is : {(num1) - (num2)}')
+# elif(operator == '*'):
+#     print(f'summation is : {(num1) * (num2)}')
+# elif(operator == '%'):
+#     print(f"summation is : {(num1) % (num2)}")
+# elif(operator == '/'):
+#     print(f"summation is : {(num1) / (num2)}")
+
+
+
+# while for
+
+# a = 0
+
+# print(a<20)
+
+# while a <= 10 :
+#     print(a)
+#     if a == 5:
+#         break
+
+#     a += 1 # a = a + 1 
+
+#  = 0
+# while a < 10 :
+#     a += 1 # a = a + 1
+#     if a == 5:
+#         continue
+#     print(a)
+
+# print('I Love Bangladesh' )        
+
+
+# a = 1
+# while a <= 10 :
+#     print('bangladesh')
+#     if a == 5:
+#         print('india')
+#     a += 1
+
+
+# a = 'i love bangladesh'
+
+
+# for rahim in a :
+#     print(rahim)
+
+
+# for i in range(10):
+#     print(i)
+# print(a[0:])
+
+# for i in range(len(a)):
+#     print(i , a[i])
+
+# x = int(input('enter a number'))
+
+# for i in range(1,11):
+#     print(f'{x} X {i} = {x * i}')
+
+# a = 1
+# while a <= 10:
+#     print(f'{x} X {a} = {x * a}')
+#     a += 1
+
+
+# while True:
+#     x = int(input('enter a number :'))
+#     if x == 0:
+#          print('thank you for using this application')
+#          break
+#     for i in range(1,11):
+#       print(f'{x} X {i} = {x * i}')
+
+
+
+# a = [54,56,'bangladesh',True,56,34]
+
+# print(a[2])
+
+# for i in a :
+# print(len(a))
+
+
+# a = [54,56,'bangladesh',True,56.34,True]
+# a . append('india')
+
+# print(a.index('bangladesh')) value index number find
+# print(a)
+
+# a.insert(2,'india')
+# a.remove(56.34)
+# a.pop(2)
+# print(a.count(True))
+# b = a.copy()
+# # b.pop(2)
+# b.clear()
+# print(a)
+# print(b)
+
+# a = [4,6,3,4,1,2]
+# a = [54,56,'bangladesh',True,56.34,True]
+# a.sort()
+# a.reverse()
+# b = [34,56,678]
+# a.extend(b) adding b to a
+# print(a)
+
+
+# print(type(b))
+# a = (54,56,'bangladesh',True,56.34,True)
+
+# print(type(a))
+
+# print(a.count(True))
+
+# b = list(a) 
+# print(type(b))
+# b.pop(2)
+# a = tuple(b)
+# print(a)
+
+
+# first breaket make = tuple
+# second breaket make = set
+# third "        "      =  list
+
+# a = {54,56,'bangladesh',True,56.34,True}
+# b = {1,2,3}
+# print(type(a))
+
+
+# a = {54,56,'bangladesh',True,56.34,True} set squance not maintain 
+# print(a)
+
+# print(a)
+
+# b = a.copy()
+# a.add('apple')
+# print(b)
+
+# a.discard('bangladesh')
+
+# a.remove('bangladesh')
+# a.pop()
+
+# a.update(b)
+# a = {2,3,4,5}
+# b = {5,8,3,2}
+# c = a.difference(b)
+# c = b.difference(a)
+# c = a.intersection(b)
+
+# c = a.union(b)
+# print(c)
+
+
+# a = { 'name':'rahim','age':34,'roll':44 }
+# print(a['age'])
+# print(a.keys())
+
+# print(a.values())
+
+# a.update({'subject':'python'})
+# a.pop('name')
+
+
+# print(a)
+
+# for i in a:
+#     print(i,a[i])
+# b = a.get('name')
+# print(b)
+
+# b = a.items()
+# print(b)
+
+
+# def rahim():
+#     print(5 + 2)
+
+# rahim()
+# rahim()
+# rahim()
+
+# def addition(a = 5,b = 4):
+#     print(a + b)
+
+# addition(5,4)
+# addition(23)
+
+# def addition(*a):
+#     return sum(a)
+
+# num = []
+
+# for i in range(5):
+#      num1 = float(input(f'enter a number {i + 1} :'))
+#      num.append (num1)
+
+# result = addition(*num)
+
+# print(result)
+
+
+# while True:
+   
+#     num1 = float(input(f'enter a number :'))
+#     if num1 != 0:
+#           num.append(num1)
+#     if num1==0:
+#      break
+
+# print(num)
+
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else :
+#         return n * factorial(n - 1)
+    
+
+# result = factorial(5)
+
+# a = int(input("enter a number :"))
+
+# print(result)
+
+# def addition(a):
+#     print(a +10)
+
+
+# result = lambda a: a+10
+
+# print(result(12))
+
+# x = lambda *a: sum(a)
+
+# print(x(12,34,56,56,56))
+
+
+# a = open('my_file.txt','r')
+
+# print(a.read())
+
+# a = open('my_file.txt','w')
+# a = open('my_file.txt','a')
+# b = '''hvhdbvsdsdsh
+# jkdsvjdbbvhfeh
+# sdbdjvbjdkvjdhvj
+
+# a.write(b)
+
+# a = open('my_file.txt','r')
+
+# print(a.read())
+
+
+# while True:
+#     for i in range(1,11):
+#         print(i)
+
+#     try:
+#         prin('hello world')
+#     except:
+#         print('this is an error')
+
+#     print('i love bangladesh')
+
+# while True:
+    
+#     try:
+#         x = int(input('enter a number :'))
+#         for i in range (1,11):
+#             print(f'{x} X {i} = {x * i}')
+#     except:
+#         print('invalid input try again')
+    
+# while True:
+    
+#     try:
+#         x = int(input('enter a number :'))
+#         for i in range (1,11):
+#             print(f'{x} X {i} = {x * i}')
+#     except Exception as e:
+#         # print('invalid input try again')
+#         print(e}
+ 
+# try:
+#     a = open('D:/batch 1/my_file.txt', 'x')
+#     try:
+#         a.write('i love bangladesh')
+#     except:
+#         print('not done')
+#     finally:
+#         a = open('D:/batch 1/my_file.txt', 'r')
+#         print(a.read())
+#         a.close()
+# except Exception as e:
+#     print(e)
+
+
+# import datetime
+# import os
+# import turtle
+# import newforlder
+# from newforlder import main
+# a = datetime.datetime.now()
+# print(a)
+
+# print(a.date())
+# print(a.year)
+# print(a.strftime('%A'))
+# print(a.strftime('%d'))
+# print(a.strftime('%d'))
+# print(a.strftime('%b'))
+# print(a.strftime('%Y'))
+# print(a.strftime('%X'))
+# print(a.strftime('%C'))
+
+
+# os.mkdir('ABCD')
+
+# os.rmdir('ABCD')
+# # a = os.listdir('.')
+# print(os.path.exists('new_name.txt'))
+
+
+
+
+# for _ in range(4):
+#     t.forward(100)
+#     t.right(90)
+
+# screen.mainloop()
+   
+
+# t.circle(100)
+# screen.mainloop()
+
+# newforlder.
+# print(x)
+# t = turtle.Turtle()
+
+
+# t.penup()
+# t.goto(-50,50)
+# t.pendown()
+# t.circle(100)
+
+# t.penup()
+# t.goto(80,-140)
+# t.pendown()
+# t.circle(100)
+# turtle.done()
+
+
+# class Myclass:
+#     a = 10
+
+#     def myfunc(self):
+#         print('hello')
+
+
+# x = Myclass()
+
+# print(x.a)
+
+# x.myfunc()
+
+
+
+# class Student:
+#     def __init__(self,name,age):
+#         self.x = name
+#         self.y = age
+#         # print(self.x,self.y)
+
+
+# student = Student('Rahim',45) 
+# print(student.x)
+# print(student.y)
+
+
+
+
+# class Namta:
+#     def myfunc(self,num):
+#         for i in range(1,11):
+#             print(f"{num} X {i} = {num* i}")
+
+# namta = Namta()
+# namta.myfunc
+
+# class Namta:
+#     def __init__(self,num):
+#         for i in range(1,11):
+#             print(f"{num} X {i} = {num* i}")
+# num2 = int(input('Enter a number:'))
+# # namta = Namta(num2)5
+
+# Namta(num2)
+
+
+# class Parents:
+#     a = 10
+#     b = 15
+#     def myfunc(self):
+#         print('hello')
+
+# class Child(Parents):
+#     x = 34
+#     y = 4554
+#     def myfunc(self):
+#         print('world')
+
+# parent = Parents()
+# # Parents.
+# child = Child()
+# child.myfunc()
+
+
+# class Dog:
+#     def speak(self):
+#         return "woof"
+        
+    
+
+# class Cat:
+#     def speak(self):
+#         return "Meow"
+        
+    
+    
+# class Cow:
+#     def speak(self):
+#         return "Moo"
+        
+
+# animal = [Dog(),Cat(),Cow()]
+
+# for i in animal:
+#     print(i.speak())
+
+
+
+# dog = Dog()
+# cat = Cat()
+# cow = Cow()
+
+# def animal_sound(animal):
+#     print(animal.speak())
+# animal_sound(dog)    
+# animal_sound(cat)    
+# animal_sound(cow)    
+    
+
+# class Student:
+#     def __init__(self,name):
+#         self.x = name
+#         print(f"{self.x} has been created")
+
+#     def __del__(self):
+#         print(f"{self.x} has been destroyed")
+
+# student = Student('Rahim')
+
+# del student
+
+
+class logger:
+    def __init__(self,filename):
+        self.file = open(filename,'w')
+        self.file.write('This is our test case')
+        print("weite started")
+
+    def log(self,message):
+        self.file.write(message+ '\n')
+
+    def __del__(self):
+        self.file.write("end"+ '\n')
+        self.file.close()
+        print("file has been closed")
+
+loger = logger('new_name.txt')
+loger.log('this is the message')
+# del loger
